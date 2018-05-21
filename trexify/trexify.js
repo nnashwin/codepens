a8e1ee3a0480917712bf86caa141bf8f646c6b28
Tyler Boright <lupin3.ken@gmail.com>
2018-05-20 18:39:33 -0700

    changes the height of the trex and uses the document.body instead of a body found through the tag

--- trexify/trexify.js
+++ trexify/trexify.js
@@ -1,10 +1,13 @@
 const trexify = () => {
-	const body = document.getElementsByTagName('body')[0]
 	const holdDiv = document.createElement('div')
+	holdDiv.id = "trexDiv"
+
 	const trexImg = document.createElement('img')
 	trexImg.src = "trex.png"
+	trexImg.style.height = "100vh"
+	trexImg.style.width = "100vw"
 	holdDiv.appendChild(trexImg)
-	body.appendChild(holdDiv)
+	document.body.appendChild(holdDiv)
 }
 
 document.addEventListener("DOMContentLoaded", trexify)
