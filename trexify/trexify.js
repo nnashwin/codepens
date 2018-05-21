1d658f652d4ed78f1d5c90b2ebfed75bb8a6522b
Tyler Boright <lupin3.ken@gmail.com>
2018-05-20 18:31:20 -0700

    adds function that makes a trex show up on the screen

--- trexify/trexify.js
+++ trexify/trexify.js
@@ -1,1 +1,10 @@
-console.log('trex')
+const trexify = () => {
+	const body = document.getElementsByTagName('body')[0]
+	const holdDiv = document.createElement('div')
+	const trexImg = document.createElement('img')
+	trexImg.src = "trex.png"
+	holdDiv.appendChild(trexImg)
+	body.appendChild(holdDiv)
+}
+
+document.addEventListener("DOMContentLoaded", trexify)
